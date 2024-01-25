f4dcaf6a48d5a6c846c4161a87c34d7235435396
Charlie-Chows <sujithkasthuri247@gmail.com>
2024-01-25 17:21:45 +0530

    Build responsive Sign In / Sign Up form

--- netflix-gpt/src/Components/Login.js
+++ netflix-gpt/src/Components/Login.js
@@ -1,26 +1,37 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { BG_URL } from "../Utils/constant"
 import Header from "./Header"
+import {useState} from "react"
 
 const Login = () => {
+
+  const [signInForm,setSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setSignInForm(!signInForm);
+  }
+
   return (
-    <div>
+     <div className="relative bg-black">
        <Header />
 
        {/* BACKGROUND IAMGE APPEAR ONLY ABOVE MEDIUM SCREENS */}
-      <div className=" relative z-10 min-h-screen bg-black  md:bg-transparent">
-        <img className="hidden md:block min-h-screen w-full h-full bg-cover bg-center" src={BG_URL} alt="Netflix Background Image On login page" />  
-        <div className="absolute inset-0 bg-black bg-opacity-50"></div>     
+       <div className="z-10 min-h-screen bg-black  md:bg-transparent">
+        <img className=" absolute hidden md:block min-h-screen w-full h-full bg-cover bg-center" src={BG_URL} alt="Netflix Background Image On login page" />  
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       </div>
 
+      {/* LOGIN FORM */}
+      <form className="absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white md:p-12 md:bg-opacity-70 md:bg-black md:w-1/3 md:rounded-md">
+        <h1 className=" font-bold text-3xl py-4">{signInForm ? "Sign In" : "Sign Up"}</h1>
+        {!signInForm && <input type="text" placeholder="Email Address" className="p-2 my-5 h-12 w-full bg-zinc-800 rounded-lg" />}
+        <input type="text" placeholder="Full Name" className="p-2 my-5 h-12 w-full bg-zinc-800 rounded-lg" />
+        <input type="password" placeholder="Password" className="p-2 my-5 h-12 w-full bg-zinc-800 rounded-lg" />
+        <button className="p-0.5 my-6 w-full h-12 rounded-lg bg-red-700">{signInForm ? "Sign In" : "Sign Up"}</button>
+        <p className="py-4  text-zinc-500"onClick={toggleSignInForm}>{signInForm ? "New to Netflix ? " : "Already a user ? "} <span className="text-white cursor-pointer hover:underline">{signInForm ? "Sign Up now" : "Sign In now"}</span> </p>
 
-      <form className="absolute z-30">
-        <input type="text" placeholder="Email Address" className="p-2 m-2 " />
-        <input type="password" placeholder="Password" className="p-2 m-2" />
-        <submit className="p-4 m-4">Sign In</submit>
       </form>
-
-    </div>
+    </div>  
   )
 }
 
