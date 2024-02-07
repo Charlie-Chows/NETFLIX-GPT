7d5609440051ff5bef44d593cc9b0a4562f1918a
Charlie-Chows <sujithkasthuri247@gmail.com>
2024-02-07 13:52:43 +0530

    Speed Insights added

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import React from "react";
 import Body from "./Components/Body";
 
@@ -5,6 +7,7 @@ function App() {
   return (
     <div>
       <Body />
+      <SpeedInsights />
     </div>
   );
 }
